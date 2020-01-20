2732006af45e5bb2874348acf1cedb345d233d9b
Roma Y <rm66mail@gmail.com>
2020-01-20 20:08:43 +0500

    html structure

--- packages/back/parser.js
+++ packages/back/parser.js
@@ -2,6 +2,55 @@ const htmlMiner = require('html-miner');
 // const createHtmlDom = require('htmldom');
 // const { parse } = require('node-html-parser');
 
+/* Html structure:
+<body>
+  …
+  <div class="container" id="start">
+    …
+    <table>…</table>
+    …
+    <div class="middle_col">
+      <!--<div class="search">// -->
+      <form id="translation" … >
+      <!--</div>// -->
+      <table width="100%">
+        <tbody>
+          …
+          <tr>
+            <td … class="gray">
+              &nbsp;
+              <a href="…">computer</a>
+              <span …>[kəm'pju:tə]</span> <em>n</em>
+              <span class="small">…</span>
+            </td>
+          </tr>
+          <tr>
+            <td class="subj" …>
+              <a href="…" title="General subject">gener.</a>
+            </td>
+            <td class="trans" …>
+              <a href="…">компьютер</a> <span …>(all need&nbsp;<i><a href="…">Буткова</a></i>)</span>;
+              <a href="…">вычислитель</a>;
+              <a href="…">счётчик</a> <span …>(прибора)</span>;
+              <a href="…">кибермашина</a>
+            </td>
+          </tr>
+          <tr>
+            <td class="subj" …>…</td>
+            <td class="trans" …>…</td>
+          </tr>
+          …
+          <tr>
+            <td … class="gray">…</td>
+          </tr>
+          <tr>
+            <td class="subj" …>…</td>
+            <td class="trans" …>…</td>
+          </tr>
+        </table>
+        …
+*/
+
 const parseMtr = function(html) {
   // const root = parse(html);
   // console.log(root.querySelectorAll('table')[1]);
